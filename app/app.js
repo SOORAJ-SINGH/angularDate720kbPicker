4ad0417c1fcccbad0fc9280131ad269152b6d32d
sooraj.singh <sooraj12002@gmail.com>
2017-10-25 23:36:08 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', [
+    '720kb.datepicker'
+]);
+
+app.controller('DatepickerDemoCtrl', function ($scope) {
+    $scope.today = new Date();
+    $scope.minDate = new Date().toString();
+    $scope.open = function () {
+        $scope.opened = true;
+    }
+});
